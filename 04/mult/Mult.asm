14fc216401142cc4b037a3b6859e7a75eadf97ee
Kameron Beamer <beamerkameron@gmail.com>
2023-11-13 17:04:50 -0800

    Added Mult functionality, or at least it doesn't crash, there must be a better way

--- 04/mult/Mult.asm
+++ 04/mult/Mult.asm
@@ -10,3 +10,54 @@
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
 // Put your code here.
+
+
+// r0 and r1 are 16 bits or less
+// if is usable here, though its syntax may be different
+// I CAN build it with repeated addition, but there is a better way to do it
+// build it the simple way if you can't figure out the better way in a timely manner
+// come back later and try to find the better way when you're done
+
+// x = R0
+@R0
+D=M
+@x
+M=D
+
+// y = R1
+@R1
+D=M
+@y
+M=D
+
+// R2 = 0
+@0
+D=A
+@R2
+M=D
+
+
+(WHILE)
+	// while y <= 0
+	@y
+	D=M
+	@END
+	D;JLE
+	
+	// R2 = x + R2
+	@x
+	D=M
+	@R2
+	M=D+M
+	// y--
+	@1
+	D=A
+	@y
+	M=M-D
+	
+	@WHILE
+	0;JMP
+(END)
+	@END
+	0;JMP
+
